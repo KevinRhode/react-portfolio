8756bb78f60ac4b774462e55ff701b58f9ac39bb
KevinRhode <kevinrhode@hotmail.com>
2023-07-01 18:21:17 -0400

    add: resume page components

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,220 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <div class="resume-form">
+      <div class="main">
+        <div class="edu-exp">
+          <h3 class="resume-title">
+            Kevin Rhode<span class="resume-subtitle">Full Stack Developer and Game Technican</span>
+          </h3>
+          {/* <form action="" target="_blank">
+            <input type="submit" class="resume-btn" value="Download">
+          </form> */}
+          <article class="objective">
+            <h4 class="resume-bold">Professional Summary</h4>
+            <p class="sm">
+              Software Developer with excellent on-site training and support
+              experience. Efficient trouble shooter with a consistent track
+              record of finding solutions to existing and new coding issues.
+              Providing a seamless experience for customers.
+            </p>
+          </article>
+          <article>
+            <div>
+              <h4 class="resume-bold">Employment History</h4>
+              <p class="resume-bold">
+                Arcade Game Technican, Craigs Cruisers OpCo
+                <span>Oct 2021 - Current</span>
+              </p>
+              <p class="resume-bold">
+                Technical Director, Craigs Cruisers Family Fun Center<span>Feb 2021 - Oct 2022</span>
+              </p>
+              <ul class="resume-list">
+                <li class="sm">
+                  Developed ASP.NET site utilizing Intercard API for seamless
+                  usage at other locations
+                </li>
+                <li class="sm">
+                  Developed/Maintained Work Order Application for Silver Lake’s
+                  Location
+                </li>
+                <li class="sm">
+                  Support various software: Advantage POS, Intercard, Ideal One
+                  POS, Embed Cardless system
+                </li>
+                <li class="sm">
+                  Install and support network systems, including workstations,
+                  servers, and infrastructure devices
+                </li>
+                <li class="sm">
+                  Provide staff assistance and training in the use of technology
+                </li>
+                <li class="sm">
+                  Ensure accurate inventory of devices, related hardware, and
+                  software equipment
+                </li>
+                <li class="sm">
+                  Support network infrastructure needed by various software
+                </li>
+                <li class="sm">
+                  Support various operating systems: Windows 10, 7, Xp,
+                  Raspberry Pi OS
+                </li>
+                <li class="sm">
+                  Coordinates, responds, tracks, and follow-up on multiple
+                  location issues with Arcade Tracker ticketing system
+                </li>
+                <li class="sm">
+                  Inventory management for multiple location of games, prizes,
+                  and parts
+                </li>
+                <li class="sm">
+                  Handle ticket issues at various locations as they arise and
+                  plan trips accordingly to manage time effectively
+                </li>
+                <li class="sm">
+                  Follow up with staff when appropriate with site related issues
+                </li>
+                <li class="sm">
+                  Inspect systems/equipment, troubleshoot faults, and replaced
+                  malfunctioning parts with new or repaired components
+                </li>
+                <li class="sm">
+                  Maintain energy and enthusiasm in fast-paced environments
+                </li>
+                <li class="sm">
+                  Perform duties in accordance with all applicable standards,
+                  policies, and regulatory guidelines to promote safe, positive,
+                  and a professional working environment
+                </li>
+                <li class="sm">
+                  Communicate proactively with guests and staff troubleshooting
+                  problems and providing guidance on proprietary
+                  systems/equipment
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p class="resume-bold">
+                Support and Development Manager,&nbsp;PUREPOS, Grandvile MI<span>May 2017 – Feb 2021</span>
+              </p>
+              <ul class="resume-list">
+                <li class="sm">
+                  Lead version control efforts for FormulaPOS, employing GitHub
+                  and other public and open-source repositories
+                </li>
+                <li class="sm">
+                  Authored code fixes and enhancements for inclusion in future
+                  code releases and patches of FormulaPOS
+                </li>
+                <li class="sm">
+                  Coordinated, responded, tracked and followed-up on customer
+                  problem reports/technical support requests
+                </li>
+                <li class="sm">
+                  Completed documentation and procedures for installation and
+                  maintenance including database modification, and deployment
+                  requirements
+                </li>
+                <li class="sm">
+                  Trained staff during demonstrations, meetings, and conferences
+                </li>
+                <li class="sm">Supported Various Point of Sale system</li>
+              </ul>
+            </div>
+            <div>
+              <p class="resume-bold">
+                Game Technican, Craigs Cruisers Family Fun Center, Wyoming
+                MI&nbsp;<span>Dec 2015 – May 2017</span>
+              </p>
+              <ul class="resume-list">
+                <li class="sm">
+                  Inspected systems, troubleshot faults and replaced
+                  malfunctioning parts with new or repaired components
+                </li>
+                <li class="sm">
+                  Maintained energy and enthusiasm in fast-paced environment
+                </li>
+                <li class="sm">
+                  Created spreadsheets in Microsoft Excel for daily, weekly and
+                  monthly reporting
+                </li>
+                <li class="sm">
+                  Performed duties in accordance with all applicable standards,
+                  policies and regulatory guidelines to promote safe working
+                  environment
+                </li>
+                <li class="sm">
+                  Managed project requirements and accomplished objectives by
+                  self-monitoring progress and promptly solving issues
+                </li>
+                <li class="sm">
+                  Communicated proactively with guests to troubleshoot problems
+                  and provide guidance
+                </li>
+                <li class="sm">
+                  Developed an Arduino based solution to automate manual process
+                </li>
+              </ul>
+            </div>
+          </article>
+          <article>
+            <h4 class="resume-bold">Education</h4>
+            <div>
+              <p class="resume-bold">Grnad Rapids Community College</p>
+              <p class="sm">Associate's Degree: Computer Programing</p>
+              <p class="sm">
+                Job Training - Certificate of Compeletion: Computer Support
+                Technician
+              </p>
+            </div>
+            <div>
+              <p class="resume-bold">Mount Pleasant High School</p>
+              <p class="sm">Diploma</p>
+            </div>
+          </article>
+          <article>
+            <h4 class="resume-bold">Contact</h4>
+            <p class="sm">Wyoming MI, 49418</p>
+            <a class="sm" href="mailto:kevinrhode@hotmail.com">KevinRhode@Hotmail.com</a>
+          </article>
+          <article class="sidebar">
+            <p class="resume-bold">Skills</p>
+            <p class="sm">Ability to Multitask</p>
+            <p class="sm">Ability to Work Under Pressure</p>
+            <p class="sm">Ability to Work in a Team</p>
+            <p class="sm">HTML5</p>
+            <p class="sm">Bootstrap</p>
+            <p class="sm">Bulma CSS</p>
+            <p class="sm">JavaScript</p>
+            <p class="sm">jQuery</p>
+            <p class="sm">Node.js</p>
+            <p class="sm">MySQL</p>
+            <p class="sm">MongoDB</p>
+            <p class="sm">Git</p>
+            <p class="sm">Microsoft Office Suite</p>
+            <p class="sm">Interpersonal Skills</p>
+            <p class="sm">Creativity</p>
+            <p class="sm">Problem Solving</p>
+            <p class="sm">Decision Making</p>
+            <p class="sm">Computer Skills</p>
+            <p class="sm">Effective Time Management</p>
+            <p class="sm">Fast Learner</p>
+            <p class="sm">Adaptability</p>
+            <p class="sm">Customer Service</p>
+            <p class="sm">Communication Skills</p>
+            <p class="sm">Detail-oriented</p>
+          </article>
+          <article class="sidebar">
+            <p class="resume-bold">Hobbies</p>
+            <p class="sm">
+              Spending Time with Friends, playing card games, playing board
+              games, playing video games, listening to music, Watching Hockey
+            </p>
+          </article>
+        </div>
+      </div>
+    </div>
+    );
+}
